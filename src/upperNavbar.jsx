22122452d57b934c06ad8e12173de89ac0440a51
alidika200 <alidika1000@gmail.com>
2024-06-16 16:41:12 +0300

    mistake

--- src/upperNavbar.jsx
+++ src/upperNavbar.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faImage, faVideo } from '@fortawesome/free-solid-svg-icons';
 import './upperNavBar.css';
+import {
+  PictureOutlined
+} from '@ant-design/icons';
+
 
 const UpperNavBar = ({ onPostTypeSelected }) => {
   return (
     <div className="upper-nav-bar">
       <button className="button-large" onClick={() => onPostTypeSelected('text')}>
-        <FontAwesomeIcon icon={faPen} />
+      <PictureOutlined /> 
       </button>
       <button className="button-large" onClick={() => onPostTypeSelected('image')}>
         <FontAwesomeIcon icon={faImage} />
